9738f6e34f1bc0194f8b55fa1cc3305ddb2b3f16
Ali Güçlü <aliguclutr@gmail.com>
2021-07-25 01:35:27 +0300

    Update classCall.php

--- src/classCall.php
+++ src/classCall.php
@@ -8,12 +8,12 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 1.8
+ * @version 1.9
  */
 
 namespace BMVC\Libs;
 
-class classCall
+trait classCall
 {
 
 	/**
