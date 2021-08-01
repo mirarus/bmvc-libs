781e805f6336794b40b9d286c6792c5428635aa1
Ali Güçlü <aliguclutr@gmail.com>
2021-08-02 00:50:11 +0300

    Update Composer.php

--- src/Composer.php
+++ src/Composer.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.3
+ * @version 0.4
  */
 
 namespace BMVC\Libs;
@@ -18,7 +18,7 @@ class Composer
 	
 	public static function folderDelete()
 	{
-		FS::rm_dir(".git");
-		FS::rm_dir(".github");
+		FS::rm_dir_sub(".git");
+		FS::rm_dir_sub(".github");
 	}
 }
