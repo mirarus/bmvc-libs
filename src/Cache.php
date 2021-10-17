8a7b7f4a6f06b96bb7abd2127f013caa01f30965
Ali Güçlü <aliguclutr@gmail.com>
2021-10-17 19:23:19 +0300

    Update Cache.php

--- src/Cache.php
+++ src/Cache.php
@@ -8,9 +8,10 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.0
+ * @version 0.1
  */
 
 namespace BMVC\Libs;
 
 final class Cache extends Cache\Cache {}
+final class Cache_Shmop extends Cache\Shmop {}
