4988c7b42566e1149c42f44b3465167a0fac66b9
Ali Güçlü <aliguclutr@gmail.com>
2022-04-22 23:10:10 +0300

    Update Whoops.php

--- src/Whoops/Whoops.php
+++ src/Whoops/Whoops.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.1
+ * @version 0.2
  */
 
 namespace BMVC\Libs\Whoops;
@@ -19,78 +19,77 @@ use Whoops\Handler\PrettyPageHandler;
 class Whoops
 {
 
-	/**
-	 * @var array
-	 */
-	private static $blacklist = [ // @phpstan-ignore-line
-		'_GET' => [],
-		'_POST' => [],
-		'_FILES' => [],
-		'_COOKIE' => [],
-		'_SESSION' => [],
-		'_SERVER' => [],
-		'_ENV' => []
-	];
+  /**
+   * @var array
+   */
+  private static $blacklist = [ // @phpstan-ignore-line
+    '_GET' => [],
+    '_POST' => [],
+    '_FILES' => [],
+    '_COOKIE' => [],
+    '_SESSION' => [],
+    '_SERVER' => [],
+    '_ENV' => []
+  ];
 
-	/**
-	 * @var string
-	 */
-	public static $environment;
+  /**
+   * @var string
+   */
+  public static $environment;
 
-	/**
-	 * @var object
-	 */
-	public static $whoops;
+  /**
+   * @var object
+   */
+  public static $whoops;
 
-	public function __construct()
-	{
-		self::init();
-	}
+  public function __construct()
+  {
+    self::init();
+  }
 
-	public static function init(): void
-	{
-		$PPH = new PrettyPageHandler;
-		foreach (self::$blacklist as $key => $val) {
-			foreach ($val as $arg) {
-				$PPH->blacklist($key, $arg);
-			}
-		}
-	
-		$whoops = new Run;
+  public static function init(): void
+  {
+    $PPH = new PrettyPageHandler;
+    foreach (self::$blacklist as $key => $val) {
+      foreach ($val as $arg) {
+        $PPH->blacklist($key, $arg);
+      }
+    }
 
-		if (self::$environment == 'development') {
-			$whoops->pushHandler($PPH);
-		}
+    $whoops = new Run;
 
-		$whoops->register();
+    if (self::$environment == 'development') {
+      $whoops->pushHandler($PPH);
+      $whoops->register();
+    }
 
-		self::$whoops = $whoops;
-	}
+    self::$whoops = $whoops;
+  }
 
-	/**
-	 * @param string $name
-	 * @param mixed  $keys
-	 */
-	public static function blacklist(string $name, $keys): void
-	{
-		if (is_array($keys)) {
-			foreach ($keys as $key) {
-				self::$blacklist[$name][] = $key;
-			}
-		} elseif (is_string($keys)) {
-			self::$blacklist[$name][] = $keys;
-		}
-	}
+  /**
+   * @param string $name
+   * @param mixed $keys
+   */
+  public static function blacklist(string $name, $keys): void
+  {
+    if (is_array($keys)) {
+      foreach ($keys as $key) {
+        self::$blacklist[$name][] = $key;
+      }
+    } elseif (is_string($keys)) {
+      self::$blacklist[$name][] = $keys;
+    }
+  }
 
-	/**
-	 * @param  string       $key
-	 * @param  string|null  $val
-	 * @param  bool|boolean $new
-	 * @return null|self
-	 */
-	public static function set(string $key, string $val = null, bool $new = false)
-	{
-		self::${$key} = $val;
-		if ($new == true) return new self;  // @phpstan-ignore-line
-	}
+  /**
+   * @param string $key
+   * @param string|null $val
+   * @param bool|boolean $new
+   * @return null|self
+   */
+  public static function set(string $key, string $val = null, bool $new = false)
+  {
+    self::${$key} = $val;
+    if ($new == true) return new self;  // @phpstan-ignore-line
+  }
 }
