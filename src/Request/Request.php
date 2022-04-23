60ab0f98744ff072d1820f41d61d0ec87085a3f5
Ali Güçlü <aliguclutr@gmail.com>
2022-04-23 14:33:17 +0300

    ...

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.4
+ * @version 0.3
  */
 
 namespace BMVC\Libs\Request;
@@ -252,9 +252,9 @@ class Request implements IRequest
 	}
 
 	/**
-	 * @return null|string
+	 * @return string
 	 */
-	public static function getContentType()
+	public static function getContentType(): string
 	{
 		return self::header('CONTENT_TYPE');
 	}
--- src/Whoops/Whoops.php
+++ src/Whoops/Whoops.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.2
+ * @version 0.1
  */
 
 namespace BMVC\Libs\Whoops;
@@ -19,77 +19,78 @@ use Whoops\Handler\PrettyPageHandler;
 class Whoops
 {
 
-  /**
-   * @var array
-   */
-  private static $blacklist = [ // @phpstan-ignore-line
-    '_GET' => [],
-    '_POST' => [],
-    '_FILES' => [],
-    '_COOKIE' => [],
-    '_SESSION' => [],
-    '_SERVER' => [],
-    '_ENV' => []
-  ];
+	/**
+	 * @var array
+	 */
+	private static $blacklist = [ // @phpstan-ignore-line
+		'_GET' => [],
+		'_POST' => [],
+		'_FILES' => [],
+		'_COOKIE' => [],
+		'_SESSION' => [],
+		'_SERVER' => [],
+		'_ENV' => []
+	];
 
-  /**
-   * @var string
-   */
-  public static $environment;
+	/**
+	 * @var string
+	 */
+	public static $environment;
 
-  /**
-   * @var object
-   */
-  public static $whoops;
+	/**
+	 * @var object
+	 */
+	public static $whoops;
 
-  public function __construct()
-  {
-    self::init();
-  }
+	public function __construct()
+	{
+		self::init();
+	}
 
-  public static function init(): void
-  {
-    $PPH = new PrettyPageHandler;
-    foreach (self::$blacklist as $key => $val) {
-      foreach ($val as $arg) {
-        $PPH->blacklist($key, $arg);
-      }
-    }
+	public static function init(): void
+	{
+		$PPH = new PrettyPageHandler;
+		foreach (self::$blacklist as $key => $val) {
+			foreach ($val as $arg) {
+				$PPH->blacklist($key, $arg);
+			}
+		}
+	
+		$whoops = new Run;
 
-    $whoops = new Run;
+		if (self::$environment == 'development') {
+			$whoops->pushHandler($PPH);
+		}
 
-    if (self::$environment == 'development') {
-      $whoops->pushHandler($PPH);
-      $whoops->register();
-    }
+		$whoops->register();
 
-    self::$whoops = $whoops;
-  }
+		self::$whoops = $whoops;
+	}
 
-  /**
-   * @param string $name
-   * @param mixed $keys
-   */
-  public static function blacklist(string $name, $keys): void
-  {
-    if (is_array($keys)) {
-      foreach ($keys as $key) {
-        self::$blacklist[$name][] = $key;
-      }
-    } elseif (is_string($keys)) {
-      self::$blacklist[$name][] = $keys;
-    }
-  }
+	/**
+	 * @param string $name
+	 * @param mixed  $keys
+	 */
+	public static function blacklist(string $name, $keys): void
+	{
+		if (is_array($keys)) {
+			foreach ($keys as $key) {
+				self::$blacklist[$name][] = $key;
+			}
+		} elseif (is_string($keys)) {
+			self::$blacklist[$name][] = $keys;
+		}
+	}
 
-  /**
-   * @param string $key
-   * @param string|null $val
-   * @param bool|boolean $new
-   * @return null|self
-   */
-  public static function set(string $key, string $val = null, bool $new = false)
-  {
-    self::${$key} = $val;
-    if ($new == true) return new self;  // @phpstan-ignore-line
-  }
+	/**
+	 * @param  string       $key
+	 * @param  string|null  $val
+	 * @param  bool|boolean $new
+	 * @return null|self
+	 */
+	public static function set(string $key, string $val = null, bool $new = false)
+	{
+		self::${$key} = $val;
+		if ($new == true) return new self;  // @phpstan-ignore-line
+	}
 }
