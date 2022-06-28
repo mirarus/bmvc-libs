c07c4ddfc5ddc84feeb16a034c121dec593576f8
Ali Güçlü <aliguclutr@gmail.com>
2022-06-28 23:13:01 +0300

    Update View.php

--- src/View/View.php
+++ src/View/View.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-core
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.3
+ * @version 0.4
  */
 
 namespace BMVC\Libs\View;
@@ -310,7 +310,7 @@ class View
   private static function _data(array &$data)
   {
     self::$data = $data = (array)array_merge($data, self::$data);
-    @extract($data);
+    //@extract($data);
     @$GLOBALS['view'] = $data;
     @$_REQUEST['view'] = $data;
   }
