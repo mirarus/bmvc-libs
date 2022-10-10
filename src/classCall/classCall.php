96c5d594d230547f4c3edd35643002ca41619b7c
Ali Güçlü <aliguclutr@gmail.com>
2022-10-10 21:13:50 +0300

    Update classCall.php

--- src/classCall/classCall.php
+++ src/classCall/classCall.php
@@ -8,7 +8,7 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.3
+ * @version 0.4
  */
 
 namespace BMVC\Libs\classCall;
@@ -79,6 +79,8 @@ trait classCall
    */
   public static function call($action, array $params = null, object &$return = null)
   {
+    $action = CL::replace($action);
+
     if (is_callable($action)) {
 
       if ($params == null) {
@@ -121,6 +123,7 @@ trait classCall
       $class = CL::replace($class);
       #
       $_class = (self::$called_class ? self::$called_class : @get_called_class());
+      $_class = CL::replace($_class);
       $_class = (new $_class);
       $_class = $_class->import($class);
 
