2d329393fdf9ff0773af303d9395965fcead4bb9
Ali Güçlü <aliguclutr@gmail.com>
2023-09-01 18:43:30 +0300

    Update Convert.php

--- src/Convert/Convert.php
+++ src/Convert/Convert.php
@@ -8,72 +8,71 @@
  * @author  Ali Güçlü (Mirarus) <aliguclutr@gmail.com>
  * @link https://github.com/mirarus/bmvc-libs
  * @license http://www.php.net/license/3_0.txt  PHP License 3.0
- * @version 0.2
+ * @version 0.3
  */
 
 namespace BMVC\Libs\Convert;
 
-use stdClass;
 use SimpleXMLElement;
+use stdClass;
 
 class Convert
 {
 
-  /**
-   * @param array $array
-   * @return stdClass
-   */
-  public static function arr_obj(array $array): stdClass
-  {
-    $object = new stdClass();
-    if (is_array($array)) {
-      foreach ($array as $key => $value) {
-        if (is_array($value)) {
-          $value = self::arr_obj($value);
-        }
-        $object->$key = $value;
-      }
-    }
-    return $object;
-  }
+	/**
+	 * @param array $array
+	 *
+	 * @return stdClass
+	 */
+	public static function arr_obj(array $array): stdClass
+	{
+		$object = new stdClass();
+		if (is_array($array)) {
+			foreach ($array as $key => $value) {
+				if (is_array($value)) {
+					$value = self::arr_obj($value);
+				}
+				$object->$key = $value;
+			}
+		}
+		return $object;
+	}
 
-  /**
-   * @param object $object
-   * @return array
-   */
-  public static function obj_arr(object $object): array
-  {
-    $array = [];
-    if (is_object($object)) {
-      foreach ($object as $key => $value) {
-        if (is_object($value)) {
-          $value = self::obj_arr($value);
-        }
-        $array[$key] = $value;
-      }
-    }
-    return $array;
-  }
+	/**
+	 * @param mixed $data
+	 *
+	 * @return array
+	 */
+	public static function obj_arr($data): array
+	{
+		$result = [];
+		foreach ($data as $key => $value) {
+			$result[$key] = (is_array($value) || is_object($value)) ? self::obj_arr($value) : $value;
+		}
+		return $result;
 
-  /**
-   * @param array $array
-   * @param object|null $xml
-   * @return bool|string
-   */
-  public static function arr_xml(array $array, object $xml = null)
-  {
-    if ($xml == null) {
-      $xml = new SimpleXMLElement('<result/>');
-    }
-    if (is_array($array)) {
-      foreach ($array as $key => $val) {
-        if (is_array($val)) {
-          self::arr_xml($val, $xml->addChild($key));
-        } else {
-          $xml->addChild($key, $val);
-        }
-      }
-    }
-    return $xml->asXML();
-  }
+	}
+
+	/**
+	 * @param array $array
+	 * @param object|null $xml
+	 *
+	 * @return bool|string
+	 */
+	public static function arr_xml(array $array, object $xml = null)
+	{
+		if ($xml == null) {
+			$xml = new SimpleXMLElement('<result/>');
+		}
+		if (is_array($array)) {
+			foreach ($array as $key => $val) {
+				if (is_array($val)) {
+					self::arr_xml($val, $xml->addChild($key));
+				} else {
+					$xml->addChild($key, $val);
+				}
+			}
+		}
+		return $xml->asXML();
+	}
 }
