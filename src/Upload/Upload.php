e8106845bff227f20854694a1f6c18cccdf2d4e0
Ali Güçlü <aliguclutr@gmail.com>
2025-03-03 21:32:34 +0300

    Update Upload.php

--- src/Upload/Upload.php
+++ src/Upload/Upload.php
@@ -19,7 +19,7 @@ class Upload
 	/**
 	 * @var array
 	 */
-	private array $error = [
+	private $error = [
 		'upload' => [
 			0 => 'There is no error, the file uploaded with success',
 			1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
